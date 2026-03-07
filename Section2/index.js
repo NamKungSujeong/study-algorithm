e7478d2725d7dfdcd8c04bd8eed52ba6ff58144c
Sujeong <nksj98@naver.com>
2026-03-07 14:59:21 +0900

    6강, 7강

--- Section2/index.js
+++ Section2/index.js
@@ -2,3 +2,30 @@
 
 // Big O 포기법이란?
 // 여러가지 코드를 일반적으로 서로 비교하고 성능을 평가하는 방법
+
+// const addUpTo_1 = (n) => {
+//   let total = 0;
+//   for (let i = 0; i <= n; i++) {
+//     total += i;
+//   }
+
+//   return total;
+// };
+
+const addUpTo_2 = (n) => {
+  return (n * (n + 1)) / 2;
+};
+
+let t1 = performance.now();
+addUpTo_2(1000000000);
+let t2 = performance.now();
+console.log(`Time : ${(t2 - t1) / 1000}seconds`);
+
+// 10억 계산 기준
+// addUpTo_1 = 0.9625496670007706seconds
+// addUpTo_2 = 0.00001087498664855957seconds
+
+// 이렇게 시간으로 비교하는건 다양한 변수와 한계가 생길 수 있음
+// 대신 컴퓨터가 처리해야하는 연산 갯수를 세면됨
+// addUpTo_2의 경우 연산이 3번이 있음 (*, +, /)
+// addUpTo_1의 경우 루프안에 있기 때문에 N갯수의 연산이 됨
